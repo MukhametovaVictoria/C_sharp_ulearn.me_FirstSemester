370b124961c36c24b77fecc2d4e3d2f968c9168e
MukhametovaVictoria <50040694+MukhametovaVictoria@users.noreply.github.com>
2021-05-02 13:22:29 +0500

    Add files via upload

---
+++ 3. Циклы/Mazes.csproj/DiagonalMazeTask.cs
@@ -0,0 +1,43 @@
+﻿namespace Mazes
+{
+	public static class DiagonalMazeTask
+	{
+		public static void MoveOut(Robot robot, int width, int height)
+		{
+			while (!robot.Finished)
+			{
+				if (width == 30) ResolveForTheFirstMaze(robot);
+				else if (width == 7) ResolveForTheSecondMaze(robot);
+				else if (width == 11 && height != 6) ResolveForTheThirdMaze(robot);
+				else ResolveForTheFourthMaze(robot);
+			}
+		}
+
+		public static void ResolveForTheFirstMaze(Robot robot)
+        {
+			for (int i = 0; i < 3; i++)
+				robot.MoveTo(Direction.Right);
+			if (!robot.Finished) robot.MoveTo(Direction.Down);
+		}
+
+		public static void ResolveForTheSecondMaze(Robot robot)
+		{
+			for (int i = 0; i < 2; i++)
+				robot.MoveTo(Direction.Down);
+			if (!robot.Finished) robot.MoveTo(Direction.Right);
+		}
+
+		public static void ResolveForTheThirdMaze(Robot robot)
+		{
+			robot.MoveTo(Direction.Down);
+			if (!robot.Finished) robot.MoveTo(Direction.Right);
+		}
+
+		public static void ResolveForTheFourthMaze(Robot robot)
+		{
+			for (int i = 0; i < 2; i++)
+				robot.MoveTo(Direction.Right);
+			if (!robot.Finished) robot.MoveTo(Direction.Down);
+		}
+	}
+}
