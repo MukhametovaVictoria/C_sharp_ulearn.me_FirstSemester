68604b4fae973bd9bb7236886a98a62b65951686
MukhametovaVictoria <50040694+MukhametovaVictoria@users.noreply.github.com>
2021-05-02 13:42:48 +0500

    Add files via upload

---
+++ 15. Не помню откуда/WindowsFormsApplication3/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbPoints = new System.Windows.Forms.ListBox();
+            this.btnGo = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbPoints
+            // 
+            this.lbPoints.FormattingEnabled = true;
+            this.lbPoints.ItemHeight = 16;
+            this.lbPoints.Location = new System.Drawing.Point(1272, 15);
+            this.lbPoints.Margin = new System.Windows.Forms.Padding(4);
+            this.lbPoints.Name = "lbPoints";
+            this.lbPoints.Size = new System.Drawing.Size(197, 532);
+            this.lbPoints.TabIndex = 0;
+            this.lbPoints.SelectedIndexChanged += new System.EventHandler(this.lbPoints_SelectedIndexChanged);
+            // 
+            // btnGo
+            // 
+            this.btnGo.Location = new System.Drawing.Point(1272, 555);
+            this.btnGo.Margin = new System.Windows.Forms.Padding(4);
+            this.btnGo.Name = "btnGo";
+            this.btnGo.Size = new System.Drawing.Size(89, 28);
+            this.btnGo.TabIndex = 1;
+            this.btnGo.Text = "Go";
+            this.btnGo.UseVisualStyleBackColor = true;
+            this.btnGo.Click += new System.EventHandler(this.btnGo_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(1369, 555);
+            this.btnClear.Margin = new System.Windows.Forms.Padding(4);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(100, 28);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1487, 598);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnGo);
+            this.Controls.Add(this.lbPoints);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbPoints;
+        private System.Windows.Forms.Button btnGo;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ 15. Не помню откуда/WindowsFormsApplication3/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        bool IsDrawLine = false;
+
+        List<Point> Points = new List<Point>();
+        Dictionary<Point, Point> Lines = new Dictionary<Point, Point>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = CreateGraphics();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (var point in Points)
+            {
+                g.FillEllipse(new SolidBrush(Color.Black), point.X - 2, point.Y - 2, 4, 4);
+            }
+
+            if (IsDrawLine)
+            {
+                foreach (var line in Lines)
+                {
+                    g.DrawLine(new Pen(Color.Black), line.Key.X, line.Key.Y, line.Value.X, line.Value.Y);
+                }
+
+            }
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Points.Add(new Point(e.X, e.Y));
+            lbPoints.Items.Add($"{e.X}:{e.Y}");
+            Invalidate();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Points.Clear();
+            lbPoints.Items.Clear();
+
+            IsDrawLine = false;
+            Lines.Clear();
+
+            Invalidate();
+        }
+
+        private void btnGo_Click(object sender, EventArgs e)
+        {
+            lbPoints.Items.Clear();
+            var localPoints = Points.OrderBy(s => s.X).ToList();
+            for (int h = 0; h < localPoints.Count; h++)
+            {
+                var x = localPoints[h].X;
+                var y = localPoints[h].Y;
+                int firstIndex = h, finalIndex = localPoints.FindLastIndex(z => z.X == x);
+                if (firstIndex != finalIndex)
+                    localPoints = Sort(firstIndex, finalIndex - firstIndex + 1, localPoints);
+                h = finalIndex;
+            }
+                for (int j = 0; j < localPoints.Count; j++)
+            {
+                var x = localPoints[j].X;
+                var y = localPoints[j].Y;
+                int firstIndex = j, finalIndex = localPoints.FindLastIndex(z => z.X == x);
+                int predY = 0;
+                if (j != 0 && firstIndex != finalIndex)
+                {
+                    predY = localPoints[j - 1].Y;
+                    if (Math.Abs(predY - y) > Math.Abs(predY - localPoints[finalIndex].Y)) 
+                        localPoints = SortReverse(firstIndex, finalIndex - firstIndex + 1, localPoints);
+                }
+                j = finalIndex;
+            }
+
+            for (int i = 0; i < localPoints.Count - 1; i++)
+            {
+                if(!Lines.ContainsKey(localPoints[i])) Lines.Add(localPoints[i], localPoints[i + 1]);
+                lbPoints.Items.Add($"{localPoints[i].X}:{localPoints[i].Y}");
+            }
+            lbPoints.Items.Add($"{localPoints[localPoints.Count - 1].X}:{localPoints[localPoints.Count - 1].Y}");
+            IsDrawLine = true;
+            Invalidate();
+        }
+
+        private static List<Point> Sort(int firstIndex, int count, List<Point> pointsss)
+        {
+            var points = pointsss;
+            var range = points.GetRange(firstIndex, count).OrderBy(d => d.Y);
+            points.RemoveRange(firstIndex, count);
+            points.InsertRange(firstIndex, range);
+            return points;
+        }
+            private static List<Point> SortReverse(int firstIndex, int count, List<Point> pointsss)
+        {
+            var points = pointsss;
+            var range = points.GetRange(firstIndex, count).ToArray();
+            for (int i = 0; i < range.Length; i++)
+            {
+                var point = points.ElementAt(firstIndex + i);
+                points.Remove(point);
+                points.Insert(firstIndex + i, range[range.Length - 1 - i]);
+            }
+            return points;
+        }
+
+        private void lbPoints_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
